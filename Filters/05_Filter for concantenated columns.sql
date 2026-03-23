1315f1c21c66b9b9c8d768404a502308650b88d6
Joyline Rencita D'souza <107092284+Joyline-Rencita@users.noreply.github.com>
2026-03-23 13:20:24 +0530

    Update 05_Filter for concantenated columns.sql

--- Filters/05_Filter for concantenated columns.sql
+++ Filters/05_Filter for concantenated columns.sql
@@ -1,1 +1,3 @@
-.
+FILTER ${ (dimension_benchmark ==="companycode_cc" && "EKPO.BUKRS ||' - '|| EKPO.BUKRS_TEXT") } = '${benchmark_1}'
+
+FILTER ${(dimension_benchmark ==="companycode_cc" && "EKPO.BUKRS ||' - '||  EKPO.BUKRS_TEXT") || (dimension_benchmark ==="companycode_cc" && "EKPO.BUKRS ||' - '||  EKPO.BUKRS_TEXT" )} = '${benchmark_1}'
